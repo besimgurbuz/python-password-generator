35fde61971f2353d797ba466b0ad01045f11cc29
Damon <mdamon.connolly@gmail.com>
2019-07-05 10:28:34 +0100

    Auto password-copy.
    This patch adds pyperclip dependancy.
    
    Now when you generate a password, pyperclip function will automatically copy the password to the clipboard for you so that it's ready to paste.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import argparse
+import pyperclip
 import generatepassword
 
-
+#command line arguments so that you can now generate passwords without touching
+#the python code itself.
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--security", help="Set the security level", type=int)
 parser.add_argument("-l", "--length", help="Set the password length", type=int)
@@ -28,3 +30,6 @@ if password[1] == 'error':
 else:
   logger.info("Generated: " + password[0])
   print(password[1]," : ",password[0])
+
+  #Copies the resulting password to the clipboard for you.
+  pyperclip.copy(password[0])
